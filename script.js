0263f3a6798ae9b09e60ad3da3f5303eeca001cc
RKanna <rkannan.balakrishnan@hotmail.com>
2023-06-01 19:03:02 +0530

    changes required for additional code

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let containerTwo = document.getElementById("next-page-container");
 containerTwo.style.display = "none";
 let containerThree = document.getElementById("next-page-container2");
 containerThree.style.display = "none";
+let containerFour = document.getElementById("next-page-container3");
+containerFour.style.display = "none";
 
 document.getElementById("paper-img").addEventListener("click", function () {
   let containerOne = document.getElementById("gradient-section");
@@ -19,4 +21,13 @@ document.getElementById("scissor-img").addEventListener("click", function () {
   let ruleSection = document.getElementById("rule-section");
   ruleSection.style.display = "none";
   containerThree.style.display = "flex";
+});
+
+document.getElementById("rock-img").addEventListener("click", function () {
+  let containerOne = document.getElementById("gradient-section");
+  let containerFour = document.getElementById("next-page-container3");
+  containerOne.style.display = "none";
+  let ruleSection = document.getElementById("rule-section");
+  ruleSection.style.display = "none";
+  containerFour.style.display = "flex";
 });
